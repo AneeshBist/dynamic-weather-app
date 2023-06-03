a81ebff3157500bc5d5b404602ef9959686b8189
AneeshBist <aneeshbist@gmail.com>
2023-06-03 14:36:29 +0530

    implementing final fixes

--- src/components/index.js
+++ src/components/index.js
@@ -2,19 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./styling/styles.css";
 
 const Component = (props) => {
+  const DEFAULT_CITY = "Dehradun";
+
   const [city, setCity] = useState(null);
-  const [search, setSearch] = useState("haridwar");
+  const [search, setSearch] = useState(DEFAULT_CITY);
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=219e680aeb5a2cab7cf3cccec33e903e`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=219e680aeb5a2cab7cf3cccec33e903e`;
       const response = await fetch(url);
       const resJson = await response.json();
-      //console.log(resJson);
-      setCity(resJson.main);
+      if (resJson.cod === "400") {
+        setSearch(DEFAULT_CITY);
+      } else {
+        setCity(resJson);
+      }
     };
     fetchApi();
-  }, [setSearch]);
+  }, [search]);
 
   return (
     <>
@@ -23,24 +28,26 @@ const Component = (props) => {
           <input
             type="search"
             className="inputField"
+            placeholder="showing default..."
             onChange={(event) => {
+              event.preventDefault();
               setSearch(event.target.value);
             }}
           />
         </div>
-        {!city ? (
-          <p> No Data Found </p>
+        {!city || city.cod === "404" ? (
+          <div className="no-data"> No Data Found </div>
         ) : (
           <div className="info">
             <h2 className="location">
               <i className="fa-solid fa-street-view"></i>
-              <p>{search}</p>
+              <p>{city.name}</p>
             </h2>
             <p id="date"></p>
-            <h1 className="temp">{city.temp}&deg;C</h1>
+            <h1 className="temp">{city.main.temp}&deg;C</h1>
             <h3 className="temp min-max">
               {" "}
-              Min {city.temp_min}&deg;C | Max {city.temp_max}&deg;C
+              Min {city.main.temp_min}&deg;C | Max {city.main.temp_max}&deg;C
             </h3>
           </div>
         )}
